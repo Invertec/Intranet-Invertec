f2d2238ba199d23182ffe2665cd866a6e978d09a
Invertec <112434597+Invertec@users.noreply.github.com>
2024-05-02 16:58:25 -0400

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
-
 class RouteServiceProvider extends ServiceProvider
 {
     /**
@@ -18,7 +15,6 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      *
@@ -29,25 +25,24 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
-        });
-    }
 
-    /**
-     * Configure the rate limiters for the application.
-     *
-     * @return void
-     */
-    protected function configureRateLimiting()
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+            });
+        }
+    
+        /**
+         * Configure the rate limiters for the application.
+         *
+         * @return void
+         */
+        protected function configureRateLimiting()
+        {
+            RateLimiter::for('api', function (Request $request) {
+                return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            });
+        }
     }
-}
